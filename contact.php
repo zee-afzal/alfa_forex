4cdbba9b7b8acb05cec7b865d63bf0ec1682bf8c
Zeeshan Afzal <zee.afzal20@gmail.com>
2021-06-07 13:14:38 +0500

    Added contact.php

---
+++ contact.php
@@ -0,0 +1,48 @@
+<?php
+$statusMsg = '';
+$msgClass = '';
+if(isset($_POST['Submit'])){
+    // Get the submitted form data
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    // Check whether submitted data is not empty
+    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
+        
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+            $msgClass = 'errordiv';
+        }else{
+            // Recipient email
+            $toEmail = 'saadullah1800@gmail.com';
+            $emailSubject = 'Contact Request Submitted at Alfa Forex by '.$name;
+            $htmlContent = '<h2>Contact Request Submitted</h2>
+                <h4>Name</h4><p>'.$name.'</p>
+                <h4>Email</h4><p>'.$email.'</p>
+                <h4>Subject</h4><p>'.$subject.'</p>
+                <h4>Message</h4><p>'.$message.'</p>';
+            
+            // Set content-type header for sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            
+            // Additional headers
+            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
+            
+            // Send email
+            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
+                $statusMsg = 'Your contact request has been submitted successfully !';
+                $msgClass = 'succdiv';
+            }else{
+                $statusMsg = 'Your contact request submission failed, please try again.';
+                $msgClass = 'errordiv';
+            }
+        }
+    }else{
+        $statusMsg = 'Please fill all the fields.';
+        $msgClass = 'errordiv';
+    }
+}
+?>
